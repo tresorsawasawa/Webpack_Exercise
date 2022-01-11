a195f670c07e5a5f2f7fb53fbe75507ada63d633
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-11 15:22:20 +0200

    Update repo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
  function component() {
-  const element = document.createElement('h1');
-  let arr = ['Hello', 'webpack'];
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  element.innerHTML = _.join(arr, ' ');
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+   return element;
  }
 
  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { template } = require('lodash');
 
  module.exports = {
    entry: {
      index: './src/index.js',
      print: './src/print.js',
    },
-  plugins: [
+   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack Exercise',
+      template: './src/index.html'
     }),
   ],
    output: {
